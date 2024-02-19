fa7d64636f0861cd88b9156f53c0365eae3c8150
joelridden <jriddenw@gmail.com>
2024-02-19 15:49:28 +1300

    basic start

---
+++ phase_arrival/gen_phase_arrival_table.py
@@ -0,0 +1,148 @@
+"""
+    Creates the phase arrival table from a directory of mseed files
+"""
+import pandas as pd
+from obspy import read
+from picker import p_phase_picker
+import multiprocessing
+import numpy as np
+from pathlib import Path
+from datetime import timedelta
+
+
+def get_p_wave(data: np.ndarray, dt):
+    """
+    Get the P wave arrival time from the data
+    """
+    wftype = "SM"  # Input wftype is strong motion
+    pflag = "n"
+    try:
+        loc = p_phase_picker(data, dt, wftype, pflag)
+    except Exception as e:
+        print(e)
+        loc = -1
+    return loc
+
+
+def process_mseed(mseed_file_chunk: list):
+    """
+    Process a chunk of mseed files and return the phase arrival data
+
+    Parameters
+    ----------
+    mseed_file_chunk : list
+        A list of mseed files to process
+    """
+    data_list = []
+    for file in mseed_file_chunk:
+        # Read the mseed file
+        mseed_data = read(str(file))
+
+        try:
+            p_comp_000 = get_p_wave(mseed_data[0].data, mseed_data[0].stats.delta)
+        except:
+            p_comp_000 = -1
+        try:
+            p_comp_090 = get_p_wave(mseed_data[1].data, mseed_data[1].stats.delta)
+        except:
+            p_comp_090 = -1
+        try:
+            p_comp_ver = get_p_wave(mseed_data[2].data, mseed_data[2].stats.delta)
+        except:
+            p_comp_ver = -1
+
+        if p_comp_ver > 1:
+            # Vertical is greater than 1 use this for the record
+            p_wave_loc = p_comp_ver
+        else:
+            # Find the max between all three components and take away 0.5
+            p_wave_loc = max(p_comp_000, p_comp_090, p_comp_ver) - 0.5
+
+        if p_wave_loc < 0:
+            continue
+        else:
+            arid = None
+            stats = mseed_data[0].stats
+
+            # Get the datetime
+            datetime = stats.starttime + timedelta(seconds=p_wave_loc)
+
+            # Get the net
+            net = stats.network
+
+            # Get the sta
+            sta = stats.station
+
+            # Get the loc
+            loc = stats.location
+
+            # Get the chan
+            chan = stats.channel
+
+            # Get the phase
+            phase = "P"
+
+            # Get the t_res
+            t_res = None
+
+            # Get the evid
+            xml_dir = file.parent.parent.parent
+            # Find the name of the xml file
+            xml_file = list(xml_dir.glob("*.xml"))[0]
+            # Get the evid from the name without the .xml
+            evid = xml_file.stem
+
+            new_data = {
+                "arid": arid,
+                "datetime": datetime,
+                "net": net,
+                "sta": sta,
+                "loc": loc,
+                "chan": chan,
+                "phase": phase,
+                "t_res": t_res,
+                "evid": evid,
+            },
+            data_list.append(new_data)
+    return data_list
+
+
+def generate_phase_arrival_table(data_dir: Path, output_dir: Path, n_procs: int):
+    """
+    Generate the phase arrival table from a directory of mseed files
+    and save it to a csv file in the output directory
+
+    Parameters
+    ----------
+    data_dir : Path
+        The top directory containing the mseed files
+        (glob is used to find all mseed files recursively)
+    output_dir : Path
+        The directory to save the phase arrival table
+    n_procs : int
+        The number of processes to use
+    """
+
+    # Find all mseed files recursively
+    mseed_files = list(data_dir.glob("**/*.mseed"))
+
+    # Split the mseed files into chunks based on the number of processes
+    file_chunks = [
+        mseed_files[i::n_procs]
+        for i in range(n_procs)
+    ]
+
+    # Initialize a multiprocessing Pool
+    with multiprocessing.Pool(processes=n_procs) as pool:
+        # Map the reading function to the file chunks
+        mseed_data_list = pool.map(process_mseed, file_chunks)
+
+    # Create the dataframe
+    df = pd.DataFrame([tup[0] for data_list in mseed_data_list for tup in data_list])
+
+    # Save the dataframe
+    df.to_csv(output_dir / "phase_arrival_table.csv", index=False)
+
+
+generate_phase_arrival_table(Path("path/to/mseed/files"), Path("path/to/output/dir"), 1)
+
---
+++ phase_arrival/picker.py
@@ -0,0 +1,164 @@
+""" P-phase Picker """
+import numpy as np
+import scipy.signal as sig
+import scipy.linalg as alg
+
+
+def p_phase_picker(x, dt, wftype, plot, Tn=0, xi=0.6, nbins=0, o='to_peak'):
+    # Check Arguments!
+    if 'sm' in wftype.lower():
+        wftype = 'sm'
+    elif 'wm' in wftype.lower():
+        wftype = 'wm'
+    elif 'na' in wftype.lower():
+        wftype = 'na'
+    else:
+        return
+
+    if Tn is 0:
+        if dt <= 0.01:
+            Tn = 0.01
+        else:
+            Tn = 0.1
+
+    if nbins is 0:
+        if dt <= 0.01:
+            nbins = round(2 / dt)
+        else:
+            nbins = 200
+
+    if 'full' in o:
+        o = 'full'
+    elif 'to_peak' in o:
+        o = 'to_peak'
+    else:
+        return
+
+    if 'wm' in wftype.lower():
+        filtflag = 1
+        flp = 0.1
+        fhp = 10.0
+    elif 'sm' in wftype.lower():
+        # Strong-motion low- and high-pass corner frequencies in Hz
+        filtflag = 1
+        flp = 0.1
+        fhp = 20.0
+    elif 'na' in wftype.lower():
+        # No bandpass filter will be applied
+        filtflag = 0
+        # detrend waveform
+        x_d = sig.detrend(x, axis=0)
+
+    x_org = x
+
+    # Normalize input to prevent numerical instability from very low amplitudes
+    x = x / np.max(np.abs(x))
+
+    # Bandpass filter and detrend waveform
+    if filtflag is not 0:
+        x_f = butter_bandpass_filter(x, flp, fhp, dt, 4)
+        x_d = sig.detrend(x_f, axis=0)
+
+    if 'to_peak' in o:
+        ind_peak = np.nonzero(np.abs(x_d) == np.max(np.abs(x_d)))
+        xnew = x_d[0:ind_peak[0][0]]
+    elif 'full' in o:
+        xnew = x_d
+
+    # Construct a fixed-base viscously damped SDF oscillator
+    omegan = 2 * np.pi / Tn  # natural frequency in radian/second
+    C = 2 * xi * omegan  # viscous damping term
+    K = omegan ** 2  # stiffness term
+    y = np.zeros((2, len(xnew)))  # response vector
+
+    # Solve second-order ordinary differential equation of motion
+    A = np.array([[0, 1], [-K, -C]])
+    Ae = alg.expm(A * dt)
+    AeB = np.dot(alg.lstsq(A, (Ae - np.identity(2)))[0], np.array((0, 1)))
+
+    for k in range(1, len(xnew)):
+        y[:, k] = np.dot(Ae, y[:, k - 1]) + AeB * xnew[k]
+
+    veloc = (y[1, :])  # relative velocity of mass
+    Edi = np.dot(2 * xi * omegan, np.power(veloc, 2))  # integrand of viscous damping energy
+
+    # Apply histogram method
+    levels, histogram, bins = state_level(Edi, nbins)
+    locs = np.nonzero(Edi > levels[0])[0]
+    indx = np.nonzero(np.multiply(xnew[0:locs[0] - 1], xnew[1:locs[0]]) < 0)[0]  # get zero crossings
+    TF = indx.size
+
+    # Update first onset
+    if TF is not 0:
+        loc = (indx[TF - 1] + 1) * dt
+    else:
+        levels, histogram, bins = state_level(Edi, np.ceil(nbins / 2))  # try nbins/2
+        locs = np.nonzero(Edi > levels[0])[0]
+        indx = np.nonzero(np.multiply(xnew[0:locs[0] - 1], xnew[1:locs[0]]) < 0)[0]  # get zero crossings
+        TF = indx.size
+        if TF is not 0:
+            loc = (indx[TF - 1] + 1) * dt
+        else:
+            loc = -1
+    return loc
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 1 / (2 * fs)  # Nyquist frequency
+    Wn = [lowcut / nyq, highcut / nyq]  # Butterworth bandpass non-dimensional frequency
+    b, a = sig.butter(order, Wn, btype='bandpass')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
+    """ Butterworth Acausal Bandpass Filter """
+    b, a = butter_bandpass(lowcut, highcut, fs, order)
+    y = sig.filtfilt(b, a, data, axis=0)
+    return y
+
+
+def state_level(y, n):
+    ymax = np.amax(y)
+    ymin = np.min(y) - np.finfo(float).eps
+
+    # Compute Histogram
+    idx = np.ceil(n * (y - ymin) / (ymax - ymin))
+    condition = np.logical_and(idx >= 1, idx <= n)
+    idx = np.extract(condition, idx)
+    s = (int(n), 1)
+    histogram = np.zeros(s)
+    for i in range(1, np.size(idx)):
+        histogram[int(idx[i]) - 1] = histogram[int(idx[i]) - 1] + 1
+
+    # Compute Center of Each Bin
+    ymin = np.min(y)
+    Ry = ymax - ymin
+    dy = Ry / n
+    bins = ymin + (np.arange(1, n) - 0.5) * dy
+
+    # Compute State Levels
+    nz = np.nonzero(histogram)[0]  # indices
+    iLowerRegion = nz[0]
+    iUpperRegion = nz[np.size(nz) - 1]
+
+    iLow = iLowerRegion
+    iHigh = iUpperRegion
+
+    # Define the lower and upper histogram regions halfway
+    # between the lowest and highest nonzero bins.
+    lLow = iLow
+    lHigh = iLow + np.floor((iHigh - iLow) / 2)
+    uLow = iLow + np.floor((iHigh - iLow) / 2)
+    uHigh = iHigh
+
+    # Upper and lower histograms
+    lHist = histogram[int(lLow):int(lHigh)]
+    uHist = histogram[int(uLow):int(uHigh)]
+
+    levels = np.zeros(2)
+    iMax = np.argmax(lHist[1, :])
+    iMin = np.argmax(uHist)
+    levels[0] = ymin + dy * (lLow + iMax + 0.5)
+    levels[1] = ymin + dy * (uLow + iMin + 0.5)
+
+    return levels, histogram, bins
