d46021af53f2631acd12e2593c659771eb50293e
joelridden <jriddenw@gmail.com>
2024-12-06 14:12:32 +1300

    ruff

--- nzgmdb/calculation/distances.py
+++ nzgmdb/calculation/distances.py
@@ -7,17 +7,17 @@ from typing import Optional
 
 import numpy as np
 import pandas as pd
-from empirical.util import estimations
 from obspy.clients.fdsn import Client as FDSN_Client
 from pyproj import Transformer
-from qcore import coordinates, geo, grid, src_site_dist
 from shapely.geometry import Point
 from shapely.geometry.polygon import LineString, Polygon
 from source_modelling import srf
 
+from empirical.util import estimations
 from nzgmdb.CCLD import ccldpy
 from nzgmdb.management import config as cfg
 from nzgmdb.management import file_structure
+from qcore import coordinates, geo, grid, src_site_dist
 
 
 def calc_fnorm_slip(
