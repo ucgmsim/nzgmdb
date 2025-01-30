cbb1f82ca230a63c8234fe59f05712cad819efc4
joelridden <jriddenw@gmail.com>
2025-01-31 10:13:37 +1300

    do pSA

--- nzgmdb/calculation/ims.py
+++ nzgmdb/calculation/ims.py
@@ -114,8 +114,8 @@ def compute_ims_for_all_processed_records(
 
     # Load the config and extract the IM options
     config = cfg.Config()
-    intensity_measures = [ims.IM[measure] for measure in config.get_value("ims")]
-    # intensity_measures = [ims.IM.AI]
+    # intensity_measures = [ims.IM[measure] for measure in config.get_value("ims")]
+    intensity_measures = [ims.IM.pSA]
     psa_periods = np.asarray(config.get_value("psa_periods"))
     fas_frequencies = np.logspace(
         np.log10(config.get_value("common_frequency_start")),
