e81ce61bf31437dfe458ffa4a272b4423e0aef39
joelridden <jriddenw@gmail.com>
2026-04-24 09:48:10 +1200

    skip missing xml

--- nzgmdb/calculation/snr.py
+++ nzgmdb/calculation/snr.py
@@ -69,6 +69,14 @@ def compute_snr_for_single_mseed(
         inventory_file = xml_dir / f"{station}.xml"
         if inventory_file.is_file():
             inventory = read_inventory(inventory_file)
+        else:
+            # No Inventory found for the station, skip the file and add to the skipped records
+            skipped_record_dict = {
+                "record_id": mseed_file.stem,
+                "reason": "Failed to find inventory information",
+            }
+            skipped_record = pd.DataFrame([skipped_record_dict])
+            return None, skipped_record
 
     # Read mseed information
     try:
